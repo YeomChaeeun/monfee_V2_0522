c40f8ec98f2b7d6a8ab59074a2cb05f152581f1d
YeomChaeEun <yce0222@gmail.com>
2020-05-27 13:13:16 +0900

    slide fade처리 일부 수정요망

--- source/code_result/js/src/monfee_main_adBanner.js
+++ source/code_result/js/src/monfee_main_adBanner.js
@@ -5,13 +5,28 @@
   var adBannerBox = $('#adBannerBox');
   var adBannerBtn = adBannerBox.find('.adBanner_btn');
   var adBtn = adBannerBtn.find('button');
-  var adBannerIndicator = adBannerBox.find('adBanner_indicaotr');
+
+  var adBannerIndicator = adBannerBox.find('.adBanner_indicator');
   var indiCatorLi = adBannerIndicator.find('li');
+
+  var adBannerBg = $('.adBanner_bg');
+  var adBannerBgUl = adBannerBg.find('ul');
+  var adBannerBgLi = adBannerBgUl.find('li');
+  
   var adBannerImg = adBannerBox.find('.adBanner_img');
   var adBannerImgWrap = adBannerImg.children('ul');
   var adBannerLi = adBannerImgWrap.find('li');
   
   //common
+
+  var n=0;
+  var btnOk = true;
+  var k;
+
+
+  adBannerBgLi.eq(n).show();
+  adBannerBgLi.eq(n).addClass('active');
+
   var liLast = adBannerLi.eq(-1).clone(true);
   adBannerImgWrap.prepend(liLast);
   adBannerLi = adBannerImgWrap.find('li');
@@ -20,18 +35,24 @@
   adBannerImgWrap.css({width:adBannerLiLen*100+'%',transform:'translateX(-' + liWidth + '%)'});
   adBannerLi.css({width:liWidth+'%'});
 
-  var n=0;
-  var btnOk = true;
-
 
   //button
   adBtn.on('click',function(e){
     e.preventDefault();
     var clickIt = $(this)[0];
+    k = n;
     if(clickIt == $('.next')[0]){
       // 다음 버튼 클릭
       btnOk = false;
       n+=1;
+
+      adBannerBgLi.eq(n).css({zIndex:5});
+      adBannerBgLi.eq(k).fadeOut(100,function(){
+        adBannerBgLi.eq(n).fadeIn();
+        adBannerBgLi.removeClass('active');
+        $(this).addClass('active');
+      });
+
       adBannerImgWrap.stop().animate({marginLeft: n*-100 + '%'}, function(){
         if(n>=adBannerLiLen-2){
           n = -1;
@@ -44,6 +65,14 @@
       btnOk = false;
       n-=1;
       adBannerImgWrap.stop().animate({marginLeft: n*-100 + '%'}, function(){
+
+        adBannerBgLi.eq(n).css({zIndex:5});
+        adBannerBgLi.eq(k).fadeOut(100,function(){
+          adBannerBgLi.eq(n).fadeIn();
+          adBannerBgLi.removeClass('active');
+          $(this).addClass('active');
+        });
+
         if(n<0){
           n=adBannerLiLen-2;
           adBannerImgWrap.css({marginLeft: n*-100 + '%'});
@@ -51,15 +80,33 @@
         btnOk = true;
       }); 
     }
+    indiCatorLi.eq(n).siblings('li').removeClass('active');
+    indiCatorLi.eq(n).addClass('active');
   });
 
 
-
   //indicator
+  indiCatorLi.find('a').on('click',function(e){
+    e.preventDefault();
+		var clickIt = $(this).parent('li');
+		var itIndex = clickIt.index();
+    k = n;
+    n = itIndex;
+    
+    adBannerBgLi.eq(n).css({zIndex:5});
+    adBannerBgLi.eq(k).fadeOut(100,function(){
+      adBannerBgLi.eq(n).fadeIn();
+      adBannerBgLi.removeClass('active');
+      $(this).addClass('active');
+    });
 
+		adBannerImgWrap.stop().animate({'marginLeft':n * -100 + '%'});
 
+		indiCatorLi.eq(n).siblings('li').removeClass('active');
+		indiCatorLi.eq(n).addClass('active');
+  });
 
-
+  
 
 
 
