aa910a8933a1c1216b5a5f0dd058749ede24f799
YeomChaeEun <yce0222@gmail.com>
2020-05-28 13:21:16 +0900

    material icon mvLength로 원하는 개수 지정해서 넘기기

--- source/code_result/js/src/monfee_main_material_icon.js
+++ source/code_result/js/src/monfee_main_material_icon.js
@@ -2,18 +2,22 @@
 
 (function($){
   // start
-
+  
   var materialBtnArea = $('.material_btn');
   var materialBtn = materialBtnArea.children('button');
 
   var materialArea = $('.material_area');
   var meterialUl = materialArea.children('ul');
   var meterialLi = meterialUl.children('li');
+  
+  var mvLength = 2; 
 
   // materialLi.eq(-1).prependTo(meterialUl);
-  var matLiLast = meterialLi.eq(-1);
+  // var matLiLast = meterialLi.eq(-1);
+  var matLiLast = meterialLi.eq(-1*(mvLength+1)).nextAll('li');
   meterialUl.prepend(matLiLast);
   meterialLi = meterialUl.find('li');
+  meterialUl.css({width:250+'%'});
   /*
   var matFirst_01 = parseInt(meterialLi.eq(0).css('width'));
   var matFirst_02 = parseInt(meterialLi.eq(0).css('marginLeft'));
@@ -22,12 +26,13 @@
   var matFirstSize = matFirst_01 + matFirst_02 + matFirst_03 + matFirst_04;
   */
  
-  var matFirstSize = meterialLi.eq(1).outerWidth(true);
-  // meterialUl.css({marginLeft:-matFirstSize+'px'});
+  var matFirstSize = meterialLi.eq(1).outerWidth(true)*mvLength;
+  meterialUl.css({marginLeft:-matFirstSize+'px'});
   meterialUl.css({position:'relative'});
 
+  /* // version 1
+
   var n = 0;
-  meterialUl.css({width:250+'%'});
   var btnView = function(){
     if(n<=0){
       materialBtnArea.children('.prev').hide();
@@ -61,7 +66,35 @@
         btnView();
       });
     }
+  });
+  */
 
+  // version 2 : 클릭시 양끝 마지막요소를 반대편끝으로 붙여서 무한으로 이동하게 만들기
+
+  // meterialUl.css({marginLeft:-matFirstSize+'px'});
+  var thisOk = true;
+  materialBtn.on('click',function(e){
+    e.preventDefault();
+    var thisIt = $(this)[0];
+    var nextBtn = materialBtnArea.children('.next')[0];
+
+    if(thisIt == nextBtn && thisOk){
+      thisOk = false;
+      meterialUl.animate({left:-matFirstSize+'px'},function(){
+        meterialLi.eq(mvLength).prevAll('li').appendTo(meterialUl);
+        meterialUl.css({left:0});
+        meterialLi = meterialUl.find('li');
+        thisOk = true;
+      });
+    }else if(thisOk){
+      thisOk = false;
+      meterialUl.animate({left:matFirstSize+'px'},function(){
+        meterialLi.eq(-1*(mvLength+1)).nextAll('li').prependTo(meterialUl);
+        meterialUl.css({left:0});
+        meterialLi = meterialUl.find('li');
+        thisOk = true;
+      });      
+    }
   });
 
   // end
