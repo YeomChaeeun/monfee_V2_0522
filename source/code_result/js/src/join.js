31def85309d8b5af9a8f93f123b5d478756ae7e2
YeomChaeEun <yce0222@gmail.com>
2020-06-04 17:49:33 +0900

    join option js로 추가

--- source/code_result/js/src/join.js
+++ source/code_result/js/src/join.js
@@ -2,10 +2,37 @@
 
 (function($){
   // strat
+  var mobileArea = $('#mobileArea');
+  var contry = [
+    '가나 +123',
+    '가봉 +241',
+    '가이아나 +592',
+    '대한민국 +82',
+    '영국 +345',
+    '미국 +90',
+    '염채은 +95',
+    '수원 +031'
+  ];
+  var contryText = '<option value=""></option>';
+  var valueResult;
 
-
-
+  var koreaSelect = contry.indexOf('대한민국 +82');
   
+  // console.log(contry[0].split('+')[1]);
+
+  for(var i=0; i<contry.length; i++){
+    mobileArea.append(contryText);
+    mobileArea.find('option').eq(i).text(contry[i]);
+    valueResult = contry[i].split('+')[1];
+    // console.log(valueResult);
+    mobileArea.find('option').eq(i).attr({'value':valueResult});
+  }
+  mobileArea.find('option').eq(koreaSelect).attr({selected:'selected'});
+  
+  var date = new Date();
+  console.log(date);
+
+
 
   // end
 })(jQuery);
